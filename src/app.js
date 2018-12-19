f59e44ebfe9eec927eb997ec13ea5fb8d5464cd5
Martin Bagshaw <martinbagshaw1@gmail.com>
2018-12-19 17:29:03 +0000

    small dom bits attempt at form

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ const helpers = require("./views/helpers/index");
 
 const app = express();
 
+// seems to not want to work
+// const bodyParser = require("body-parser");
+
+
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.engine(
@@ -25,6 +30,26 @@ app.engine(
 app.use(
   express.static(path.join(__dirname, '..', '/public'), {maxAge: '30d'})
 );
+
+
+
+
+// ____________
+// post routes
+
+
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({ extended: false}));
+
+
+// create profile POST
+app.post("/create-profile"), (req, res) => {
+  console.log('req.body');
+  // console.log(req.body);
+  // res.redirect("/search-profiles");
+}
+
+
 
 app.set("port", process.env.PORT || 1989);
 app.use(routes);
