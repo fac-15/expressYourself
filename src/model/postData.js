956f7cebe20458025c7bcd1010db1d2da4eb7633
mike <boimik87@gmail.com>
2018-12-20 14:40:45 +0000

    fix db connection from databaseconnection

--- src/model/postData.js
+++ src/model/postData.js
@@ -1,35 +1,34 @@
-const dbConnection = require('../db/db_connection');
+const dbConnection = require("../../db/db_connection");
 
-
-const postDataLanguages = (user_id, html, css, js, sql, node, cb) => {
-    if (err) console.log(err);
-    databaseConnection.query(
-        'INSERT INTO users (user_id, html, css, js, sql, node) VALUES ($1, $2, $3, $4, $5, $6)',
-        (error, res) => {
-            if(err) {
-                return cb(error);
-            } else {
-                cb(null, res);
-            }
-        }
-    );     
-}
+const postDataLanguages = (html, css, js, sql, node, cb) => {
+  if (err) console.log(err);
+  dbConnection.query(
+    "INSERT INTO users (user_id, html, css, js, sql, node) VALUES ($1, $2, $3, $4, $5, $6)",
+    (error, res) => {
+      if (err) {
+        return cb(error);
+      } else {
+        cb(null, res);
+      }
+    }
+  );
+};
 
 const postDataUser = (name, bio, cb) => {
-        if (err) console.log(err);
-        databaseConnection.query(
-            'INSERT INTO users (name, password) VALUES ($1, $2)',
-            (error, res) => {
-                if(err) {
-                    return cb(error);
-                } else {
-                    cb(null, res);
-                }
-            }
-        );     
-}
+  if (err) console.log(err);
+  databaseConnection.query(
+    "INSERT INTO users (name, password) VALUES ($1, $2)",
+    (error, res) => {
+      if (err) {
+        return cb(error);
+      } else {
+        cb(null, res);
+      }
+    }
+  );
+};
 
 module.exports = {
-    postDataLanguages,
-    postDataUser,
-}
+  postDataLanguages,
+  postDataUser
+};
