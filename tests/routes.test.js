1a0d638013baf751bed04bfaf6c8b228f509b1b1
mike <boimik87@gmail.com>
2018-12-20 11:31:44 +0000

    routes testing

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -3,15 +3,61 @@ const request = require("supertest");
 const app = require("../src/app");
 // const server = require("../app");
 
-test("Check tape is working", t => {
+//routes tests
+//** 200 HOME **
+test("Check home route returns a 200", t => {
   request(app)
-    .get("/home")
+    .get("/")
     .expect(200)
     .end((err, res) => {
       if (err) {
         console.log(err);
       } else {
         t.equal(res.status == 200, true);
+        t.end();
+      }
+    });
+});
+
+//** 200 SEARCH-PROFILES **
+test("Check search-profiles route returns a 200", t => {
+  request(app)
+    .get("/search-profiles")
+    .expect(200)
+    .end((err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        t.equal(res.status == 200, true);
+        t.end();
+      }
+    });
+});
+
+//** NON EXISTING 404 **
+test("Check non-existing route returns a 404", t => {
+  request(app)
+    .get("/elephant-route")
+    .expect(404)
+    .end((err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        t.equal(res.status == 404, true);
+        t.end();
+      }
+    });
+});
+
+test("Check non-existing route returns a 404", t => {
+  request(app)
+    .get("/elephant-route")
+    .expect(404)
+    .end((err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        t.equal(res.status == 404, true);
         t.end();
       }
     });
